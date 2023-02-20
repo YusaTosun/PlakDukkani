2a8b00a128b403fba5b4230803765dd00738b36f
byska <beyasak@hotmail.com>
2023-02-20 16:00:00 +0300

    //dgvisim ve buton bağlantıları

--- PlakDukkaniUI/AlbumSayfasi.cs
+++ PlakDukkaniUI/AlbumSayfasi.cs
@@ -19,7 +19,40 @@ namespace PlakDukkaniUI
 
         private void AlbumSayfasi_Load(object sender, EventArgs e)
         {
+            btnSatisDurmus.Click += Click;
+            btnSatisDevamEden.Click += Click;
+            btnEnYeni10.Click += Click;
+            btnIndirimliAlbum.Click += Click;
+        }
+        private void Click(object sender, EventArgs e)
+        {
+            Button buton = (Button)sender;
+            object tag = buton.Tag;
+            switch(tag)
+            {
+                case "1": SatisDurmus(); break;
+                case "2":SatisDevam(); break;
+                case "3":EnYeniOn(); break;
+                case "4":IndirimliAlbum(); break;
+            }
+        }
 
+        private void IndirimliAlbum()
+        {
+            
+        }
+
+        private void EnYeniOn()
+        {
+        }
+
+        private void SatisDevam()
+        {
+        }
+
+        private void SatisDurmus()
+        {
+            
         }
     }
 }
